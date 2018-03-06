f2dda50da2aa9c4fbd7710827ead6afd72666a0e
Josh Lee <joshswl@gmail.com>
2018-03-05 19:51:35 -0600

    console logging postId

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
+// import comments
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    console.log(i);
+    // get us the post
     return (
       <div className="single-photo">
       I'm the single
