c5dfdb23d4f3e2b27623a6ff20f075a2978081d2
Josh Lee <joshswl@gmail.com>
2018-03-05 20:17:23 -0600

    comments being loaded in console

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Comments = React.createClass({
+    renderComment(comment, i) {
+        console.log(comment)
+    },
+
     render() {
         return (
             <div className="comment">
-                I'm the comments
+                {this.props.postComments.map(this.renderComment)}
             </div>
         )
     }
-})
+});
+
+export default Comments;
