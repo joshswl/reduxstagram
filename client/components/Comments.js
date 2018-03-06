4367f11db2dc880c2c2271fc21ea6ccc25f3f289
Josh Lee <joshswl@gmail.com>
2018-03-05 19:58:29 -0600

    finished displaying Single component - set up Comments component

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                I'm the comments
+            </div>
+        )
+    }
+})
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Photo from './Photo';
-// import comments
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
     // index of the post
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
-    console.log(i);
-    // get us the post
+    const post = this.props.posts[i]; 
+
     return (
       <div className="single-photo">
-      I'm the single
+        <Photo i={i} post ={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
