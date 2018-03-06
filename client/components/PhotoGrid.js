5abdd51003dadfe1c436dff891a9b717efbea538
Josh Lee <joshswl@gmail.com>
2018-03-05 18:44:58 -0600

    'Photo' component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-      I'm the photo grid
+        {this.props.posts.map((post, i) => <Photo />  )} 
       </div>
     )
   }
