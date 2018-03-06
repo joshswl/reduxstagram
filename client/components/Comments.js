0e5037c428ef7c02b49c074730cee7c832b825a6
Josh Lee <joshswl@gmail.com>
2018-03-05 20:21:13 -0600

    comments being loaded HTML

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Comments = React.createClass({
     renderComment(comment, i) {
-        console.log(comment)
+        return (
+            <div className="comment" key={i}>
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                    <button className="remove-comment">&times;</button>
+                </p>
+            </div>
+        )
     },
 
     render() {
         return (
-            <div className="comment">
+            <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
             </div>
         )
