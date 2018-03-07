0f3a3c6ad9e1d4196acf4f5f02f7f145c729eb81
Josh Lee <joshswl@gmail.com>
2018-03-07 00:08:07 -0600

    using Sentry for tracking errors from the user's browser

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -15,6 +15,9 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
